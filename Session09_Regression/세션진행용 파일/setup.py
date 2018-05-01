66f8b2c423620769fbbb5171d36049a743088d79
JackNKong <32261900+JackNKong@users.noreply.github.com>
2018-05-01 12:01:43 +0900

    20180501

---
+++ Session09_Regression/세션진행용 파일/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="LinearAlgebra",
+version='1.4.1',
+py_modules=['LinAlg'],
+author='Noah Rossignol',
+author_email='noahrossignol@yahoo.com',
+description='Tools for working with matrices and vectors.')
